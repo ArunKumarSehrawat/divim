30d2154d707639a2053e910cfcb6a70b7c439dda
Arun Sehrawat <arunksehrawat@gmail.com>
2023-05-21 02:04:49 +0530

    Basic setup

---
+++ divim.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/** Documentations
+ * Chalk: https://github.com/chalk/chalk
+ * Enquirer: https://github.com/enquirer/enquirer
+ * Listr: https://github.com/SamVerschueren/listr
+ * Boxen: https://github.com/sindresorhus/boxen
+ * Ora: https://github.com/sindresorhus/ora
+ */
+
+
+import "dotenv/config";
+import Chalk from "chalk";
+
+console.log(Chalk.bgBlue("workingggg..."));
